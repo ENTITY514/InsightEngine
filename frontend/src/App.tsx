d4e2be1b250d3353fbb90caa12a7fbcf9b2febee
cortezz2001 <cortezz2001@gmail.com>
2025-09-14 11:56:21 +0500

    tailwind styles setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,10 @@
+// ./src/App.js
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl text-white font-bold">Hello, Tailwind CSS!</h1>
     </div>
   );
 }
